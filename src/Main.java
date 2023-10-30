8aded61c36efe89917f78e30530286ef22000564
yxg008 <yingfa008@gmail.com>
2023-10-30 08:25:48 -0700

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-
-// Main class to test the thread pool
 public class Main {
-    public static void main(String[] args) {
-        ThreadPool pool = new ThreadPool(7);  // Create a thread pool with 7 threads
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool(10);  // Initialize a thread pool with 10 threads
 
-        // Add 5 tasks to the thread pool
-        for (int i = 0; i < 5; i++) {
+        // Add 100 tasks to the thread pool
+        for (int i = 0; i < 100; i++) {
             Task task = new Task(i);
-            pool.add(task);
+            threadPool.add(task);
         }
 
-        pool.shutdown();  // Shut down the thread pool
+        Thread.sleep(5000);  // Wait for 5 seconds before shutting down the thread pool
+        threadPool.shutdown();  // Shut down the thread pool
     }
+    /*
+    This Main class serves as a simple demonstration of how to use the ThreadPool class to execute a batch of tasks asynchronously.
+    It initializes a thread pool, adds 100 tasks to it, waits for 5 seconds to let some tasks get processed,
+    and then shuts down the thread pool.
+     */
 }
--- src/ThreadPool.java
+++ src/ThreadPool.java
@@ -34,6 +34,13 @@ public class ThreadPool {
         }
     }
 
+    // if (!isStopped) : Checks if the thread pool is still running. If it's stopped, the method does nothing.
+    // This is essential because multiple threads might try to add tasks to the queue simultaneously.
+    //queue.add(task);: Adds the Runnable task to the LinkedBlockingQueue.
+    // queue.notify();: Wakes up one of the waiting worker threads to indicate that a new task is available for execution.
+    //The method is thread-safe due to the synchronized block. It adds a task to the queue and notifies a potentially waiting worker
+    // thread that a task is available.
+
     // Method to stop the thread pool
     public void shutdown() {
         isStopped = true;  // Set the flag to true
@@ -41,23 +48,33 @@ public class ThreadPool {
             workerThread.interrupt();  // Interrupt each worker thread
         }
     }
+    /*
+    This flag is checked by the worker threads in their main loop, and if it is true, they exit the loop and effectively finish their execution.
+    for (PoolWorker workerThread : threads) : Iterates through each worker thread in the thread array.
+    workerThread.interrupt();: Sends an interrupt signal to each worker thread. This helps to break any blocking operations
+    that might be in progress, such as queue.wait().
+    It tells worker threads to stop after completing any task they are currently running.
+    It also interrupts all worker threads to force them to stop immediately.
+     */
 
     // Inner class for worker threads
     private class PoolWorker extends Thread {
         public void run() {
-            Runnable task;  // To hold the current task
+            Runnable task;  // hold the task fetched from the queue that the worker thread will execute.
 
             // Main worker loop
             while (!isStopped) {  // Keep running until the pool is stopped
-                synchronized (queue) {  // Lock the task queue
+                synchronized (queue) {  // prevent race conditions.
+                    // This is essential because multiple threads might try to fetch or add tasks to the queue at the same time.
 
-                    // Wait if the queue is empty
+                    // if the queue is empty and the pool has not been stopped, the thread goes into a waiting state
                     while (queue.isEmpty() && !isStopped) {
                         try {
-                            queue.wait();  // Wait for a task
+                            queue.wait();  // thread waits until it's notified that a new task has been added to the queue.
                         } catch (InterruptedException e) {
                             if (isStopped) {
-                                return;  // Exit if the pool is stopped
+                                return;  // Catches interruptions, which might be caused when shutdown() is called
+                                // Exit if the pool is stopped
                             }
                         }
                     }
@@ -66,13 +83,19 @@ public class ThreadPool {
                 }
 
                 // Execute the task
+                // Checks if a task was successfully fetched from the queue.
                 if (task != null) {
                     try {
                         task.run();
                     } catch (RuntimeException e) {
                         System.out.println("Thread pool is interrupted due to an issue: " + e.getMessage());
+                        // Any exceptions that are thrown during the execution of the task are caught here, and an error message is printed to the console
                     }
                 }
+                /*
+                The run() method is designed to continuously take tasks from the queue and execute them.
+                The method carefully synchronizes access to the shared task queue and properly handles interruptions and exceptions.
+                 */
             }
         }
     }
